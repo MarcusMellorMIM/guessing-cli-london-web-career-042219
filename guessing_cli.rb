5cb86b620952b9e0151d4213ebb0cc443936c6d3
Marcus Mellor <marcus@mellor.com>
2019-03-28 13:09:10 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -7,20 +7,25 @@ end
 
 def run_guessing_game
 
+  continue=true
   computer_choice=0
-  puts "Guess a number between 1 and 6."
-  choice=gets.chomp
 
-  if choice >= "1" && choice <= "6"
-    computer_choice=get_computer_choice.to_s
-    if choice==computer_choice
-      puts "You guessed the correct number!"
+  while continue
+    puts "Guess a number between 1 and 6."
+    choice=gets.chomp
+    if choice >= "1" && choice <= "6"
+      computer_choice=get_computer_choice.to_s
+      if choice==computer_choice
+        puts "You guessed the correct number!"
+      else
+        puts "The computer guessed #{computer_choice}."
+      end
+    elsif choice== "exit"
+      puts "Goodbye!"
+      continue=false
     else
-      puts "The computer guessed #{computer_choice}."
+      puts "Valid choices are 1 to 6"
     end
-  elsif choice== "exit"
-    puts "Goodbye!"
-  else
-    puts "Valid choices are 1 to 6"
   end
+
 end
